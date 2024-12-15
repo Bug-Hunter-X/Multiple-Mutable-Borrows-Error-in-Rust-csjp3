8375a07ff9633d2cd69cfcf332e275b23cae793f
BugHunter-X <askzenai@gmail.com>
2024-12-15 19:42:05 +0100

    Add bugSolution.rs file

---
+++ bugSolution.rs
@@ -0,0 +1,1 @@
+fn main() {    let mut x = 5;    {        let y = &mut x; // y is a mutable reference to x        *y = 10; // Modifying x through y    }    {        let z = &mut x; // z is a mutable reference to x        *z = 20; // Modifying x through z    }} 
